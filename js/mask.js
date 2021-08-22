05bf1442b9d58598529e5d58167dd088542fe10c
Gabriel Porfiro <contato@gabrielporfiro.com>
2021-08-22 11:42:49 -0300

    Normalizando CNPJ

--- js/mask.js
+++ js/mask.js
@@ -18,7 +18,7 @@
         e.target.value = !x[2] ? x[1] : '' + x[1] + x[2] + (x[3] ? '-' + x[3] : '');
       });
   }
-  function maskCNPJ(){
+  function maskCnpj(){
     document.getElementById('cnpj').addEventListener('input', function (e) {
         var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})(\d{0,2})/);
         e.target.value = !x[2] ? x[1] : '' + x[1] + '.' + x[2] + (x[3] ? '.' + x[3] : '') + (x[4] ? '/' + x[4] : '') + (x[5] ? '.' + x[5] : '');
@@ -28,7 +28,7 @@
       maskNumber();
       maskCpf();
       maskCep();
-      maskCNPJ();
+      maskCnpj();
   }
 
   initApps();
