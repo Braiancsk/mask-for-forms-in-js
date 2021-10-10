4df7cd90f70fc54bdda166ca233bb60480eb1038
Braian Fellipe <74879806+Braiancsk@users.noreply.github.com>
2021-10-10 10:10:32 -0300

    Update mask.js

--- js/mask.js
+++ js/mask.js
@@ -1,9 +1,9 @@
-         function maskNumber(){
+        function maskNumber(){
     document.getElementById('telefone').addEventListener('input', function (e) {
-        var x = e.target.value.replace(/\D/g, '').match(/(\d{0,0})(\d{0,2})(\d{0,1})(\d{0,4})/);
-        e.target.value = !x[2] ? x[1] : '+' + x[2] + (x[3] ? ' ' + x[3] : '') + (x[4] ? ' ' + x[4] : '') + (x[4] ? '-' + x[4] : '');
-      });
-  }
+        var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,9})/);
+        e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+    });
+}
 
   function maskCpf(){
         document.getElementById('cpf').addEventListener('input', function (e) {
