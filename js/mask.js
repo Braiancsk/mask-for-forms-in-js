1cd0d8727c1b5e38cb4a4107e64f8252368bee95
Braian Fellipe <74879806+Braiancsk@users.noreply.github.com>
2021-11-11 11:37:12 -0300

    Update mask.js

--- js/mask.js
+++ js/mask.js
@@ -1,6 +1,6 @@
         function maskNumber(){
     document.getElementById('telefone').addEventListener('input', function (e) {
-        var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,9})/);
+        var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
         e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
     });
 }
